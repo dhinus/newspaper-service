0a662de46b8e581816286ea7debb6823b39b9c89
Karl Sutt <karl@sutt.ee>
2014-07-17 10:29:14 +0100

    Initial app

---
+++ README.md
@@ -0,0 +1,14 @@
+# Juicer MKII
+
+The new, Python-based juicer
+
+## Installation
+
+You'll need `lxml`, `libxml2-dev` and `libxslt-dev` for parsing.
+
+For images, `libjpeg-dev`, `zlib1g-dev`, `libpng12-dev`.
+
+The repo comes with `requirements.txt` file for PIP, so you can just do
+`pip install -r requirements.txt` to install all the Python dependencies.
+
+See http://newspaper.readthedocs.org/en/latest/user_guide/install.html
---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+from newspaper import Article
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world"
+
+@app.route("/api/article")
+def article():
+    url = request.args.get('url')
+    print url
+    if url is None:
+        return "Please provide the url (?url=[your url])"
+
+    article = Article(url=url)
+    article.download()
+    article.parse()
+    # article.nlp()
+    return jsonify(url=url,
+                   title=article.title,
+                   authors=article.authors,
+                   description=article.meta_description,
+                   body=article.text,
+                   image=article.top_image,
+                   images=article.images,
+                   videos=article.movies,
+                   published=article.published_date)
+
+if __name__ == '__main__':
+    app.run(debug=True)
