deb150dd8caea2f30eb6dea6d6ad6259e6aac22b
Karl Sutt <karl@sutt.ee>
2014-08-19 14:28:53 +0100

    Debugged app

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from newspaper import Article
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def index():
@@ -30,4 +31,4 @@ def article():
                    meta=article.meta_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
