d4bab7b482f98afbdda1711051fdcf8b278dc010
Karl Sutt <karl@sutt.ee>
2015-02-04 14:05:50 +0000

    newspaper3k

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ from flask import Flask, request, jsonify
 from newspaper import Article
 from multiprocessing import Pool
 
-pool_size = 16
-image_processing_pool = Pool(pool_size)
-print "Using pool size of %d" % pool_size
-
 app = Flask(__name__)
 app.debug = True
 
@@ -19,19 +15,15 @@ def article():
     if url is None:
         return "Please provide the url (?url=[your url])"
 
-    article = Article(url=url, pool=image_processing_pool)
+    article = Article(url=url)
     article.download()
     article.parse()
-    # article.nlp()
     return jsonify(url=url,
                    title=article.title,
                    authors=article.authors,
                    description=article.meta_description,
                    body=article.text,
                    image=article.top_image,
-                   # images=article.images,
-                   images=article.image_sizes,
-                   videos=article.movies,
                    published=article.published_date,
                    meta=article.meta_data)
 
